d85713b742403877a3d6b1345b01f456cc384704
ahmadrifai <rifai0850@gmail.com>
2022-03-05 10:35:33 +0700

    first commit

---
+++ src/main/java/com/pelayanan/sistempelayanan/SistemPelayananApplication.java
@@ -0,0 +1,13 @@
+package com.pelayanan.sistempelayanan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemPelayananApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemPelayananApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pelayanan/sistempelayanan/SistemPelayananApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelayanan.sistempelayanan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemPelayananApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
